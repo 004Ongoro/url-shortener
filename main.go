12f24231a3cada1121d9692ce28a33fa40ae9921
George Ongoro <157955004+004Ongoro@users.noreply.github.com>
2025-02-01 12:01:34 +0300

    Update main.go

--- main.go
+++ main.go
@@ -30,7 +30,7 @@ func shortenHandler(w http.ResponseWriter, r *http.Request){
 	shortCode := GenerateShortCode()
 	urlStore[shortCode] = longUrl
 
-	fmt.Fprintf(w, "http://localhost:10000/%s", shortCode)
+	fmt.Fprintf(w, "https://gurulo/%s", shortCode)
 }
 
 func redirectHandler(w http.ResponseWriter, r *http.Request){
